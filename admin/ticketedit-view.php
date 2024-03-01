7321df7fdb30ebff09486c59c44ee7d8b20cadb7
joseph <josephmoran@duck.com>
2024-03-01 16:12:26 -0500

    se logró agregar volver en el caso de sesión admin y sesión técnico en ticketedit

--- admin/ticketedit-view.php
+++ admin/ticketedit-view.php
@@ -105,6 +105,188 @@
     // $selticket=mysqli_query($conexion,$sql_cliente);
     // if(mysqli_num_rows($selticket)>0):
 
+
+      if ($_SESSION['tipo'] == 'admin') {
+        // un cliente hizo el registro del ticket
+      $stmt = $conexion->prepare("SELECT ticket.*, cliente.*, tecnico.* FROM ticket INNER JOIN cliente ON ticket.id_cliente = cliente.id_cliente INNER JOIN tecnico ON ticket.id_tecnico = tecnico.id_tecnico WHERE ticket.id = ? AND ticket.id_cliente IS NOT NULL");
+      $stmt->bind_param("s", $id);
+      $stmt->execute();
+      $result = $stmt->get_result();
+  
+      if ($result->num_rows > 0) {
+        
+        $reg = $result->fetch_assoc();
+    
+              if ($_SESSION['tipo'] == 'tecnico') {
+                $readonly = 'disabled';
+            } else {
+                $readonly = '';
+            }
+      ?>
+            
+  
+            <!--************************************ Page content******************************-->
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-3">
+                    <img width="50" src="./img/Edit.png" alt="Image" class="img-responsive animated tada">
+                </div>
+                <div class="col-sm-9">
+                    <a href="./admin.php?view=ticketadmin" class="btn btn-primary btn-sm pull-right"><i class="fa fa-reply"></i>&nbsp;&nbsp;Volver administrar Tickets </a>
+                </div>
+              </div>
+            </div>
+                
+                
+              <div class="container">
+                <div class="col-sm-12">
+                    <form class="form-horizontal" role="form" action="" method="POST">
+                        <input type="hidden" name="id_edit" value="<?php echo $reg['id']?>">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Fecha</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <input class="form-control" readonly="" type="text" name="fecha_ticket" readonly="" value="<?php echo $reg['fecha']?>">
+                                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Serie</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <input class="form-control" readonly="" type="text" name="serie_ticket" readonly="" value="<?php echo $reg['serie']?>">
+                                        <span class="input-group-addon"><i class="fa fa-barcode"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Estado</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <select class="form-control" name="estado_ticket">
+                                            <option value="<?php echo $reg['estado_ticket']?>"><?php echo $reg['estado_ticket']?> (Actual)</option>
+                                            <option value="Pendiente">Pendiente</option>
+                                            <option value="En proceso">En proceso</option>
+                                            <option value="Resuelto">Resuelto</option>
+                                            <option value="Anulado">Anulado</option>
+                                          </select>
+                                        <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+  
+                            <div class="form-group">
+                              <label  class="col-sm-2 control-label">Nombre</label>
+                              <div class="col-sm-10">
+                                  <div class='input-group'>
+                                      <input type="text" readonly="" class="form-control"  name="name_ticket" readonly="" value="<?php echo $reg['nombre_usuario']?>">
+                                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                  </div>
+                              </div>
+                            </div>
+  
+                            <div class="form-group">
+                              <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                              <div class="col-sm-10">
+                                  <div class='input-group'>
+                                      <input type="email" readonly="" class="form-control"  name="email_ticket" readonly="" value="<?php echo $reg['email_cliente']?>">
+                                    <span class="input-group-addon"><i class="fa fa-envelope-o"></i></span>
+                                  </div> 
+                              </div>
+                            </div>
+      <?php
+      //un admin hizo el registro
+      } elseif($sql = Mysql::consulta("SELECT ticket.*, administrador.*, tecnico.* FROM ticket INNER JOIN administrador ON ticket.id_admin = administrador.id_admin INNER JOIN tecnico ON ticket.id_tecnico = tecnico.id_tecnico WHERE ticket.id = '$id' && ticket.id_admin IS NOT NULL")) {
+        $reg = mysqli_fetch_array($sql, MYSQLI_ASSOC);
+    
+            if ($_SESSION['tipo'] == 'tecnico') {  
+              $readonly = 'disabled';
+          } else {
+              $readonly = '';
+          }
+  
+          ?>
+  
+           <!--************************************ Page content******************************-->
+           <div class="container">
+              <div class="row">
+                <div class="col-sm-3">
+                    <img width="50" src="./img/Edit.png" alt="Image" class="img-responsive animated tada">
+                </div>
+                <div class="col-sm-9">
+                    <a href="./admin.php?view=ticketadmin" class="btn btn-primary btn-sm pull-right"><i class="fa fa-reply"></i>&nbsp;&nbsp;Volver administrar Tickets </a>
+                </div>
+              </div>
+            </div>
+                
+                
+              <div class="container">
+                <div class="col-sm-12">
+                    <form class="form-horizontal" role="form" action="" method="POST">
+                        <input type="hidden" name="id_edit" value="<?php echo $reg['id']?>">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Fecha</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <input class="form-control" readonly="" type="text" name="fecha_ticket" readonly="" value="<?php echo $reg['fecha']?>">
+                                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Serie</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <input class="form-control" readonly="" type="text" name="serie_ticket" readonly="" value="<?php echo $reg['serie']?>">
+                                        <span class="input-group-addon"><i class="fa fa-barcode"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Estado</label>
+                                <div class='col-sm-10'>
+                                    <div class="input-group">
+                                        <select class="form-control" name="estado_ticket">
+                                            <option value="<?php echo $reg['estado_ticket']?>"><?php echo $reg['estado_ticket']?> (Actual)</option>
+                                            <option value="Pendiente">Pendiente</option>
+                                            <option value="En proceso">En proceso</option>
+                                            <option value="Resuelto">Resuelto</option>
+                                            <option value="Anulado">Anulado</option>
+                                          </select>
+                                        <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+  
+                            <div class="form-group">
+                              <label  class="col-sm-2 control-label">Nombre</label>
+                              <div class="col-sm-10">
+                                  <div class='input-group'>
+                                      <input type="text" readonly="" class="form-control"  name="name_ticket" readonly="" value="<?php echo $reg['nombre_admin']?>">
+                                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                  </div>
+                              </div>
+                            </div>
+  
+                            <div class="form-group">
+                              <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                              <div class="col-sm-10">
+                                  <div class='input-group'>
+                                      <input type="email" readonly="" class="form-control"  name="email_ticket" readonly="" value="<?php echo $reg['email_admin']?>">
+                                    <span class="input-group-addon"><i class="fa fa-envelope-o"></i></span>
+                                  </div> 
+                              </div>
+                            </div>
+  
+        <?php  
+      }
+      
+    } elseif ($_SESSION['tipo'] == 'tecnico') {
       // un cliente hizo el registro del ticket
     $stmt = $conexion->prepare("SELECT ticket.*, cliente.*, tecnico.* FROM ticket INNER JOIN cliente ON ticket.id_cliente = cliente.id_cliente INNER JOIN tecnico ON ticket.id_tecnico = tecnico.id_tecnico WHERE ticket.id = ? AND ticket.id_cliente IS NOT NULL");
     $stmt->bind_param("s", $id);
@@ -283,8 +465,14 @@
 
       <?php  
     }
+    
+  }
    
     ?>
+
+
+
+
 
 
           
